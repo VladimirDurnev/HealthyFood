0e48fae04a1914a1481c151e2fab12bd5c62ca18
Your Name <durak2754@yadnex.ru>
2024-02-05 01:02:04 +0400

    add static, Sort.tsx, debounce + fix searchSlice

--- src/Redux/searchSlice.ts
+++ src/Redux/searchSlice.ts
@@ -3,29 +3,66 @@ import axios from "axios";
 import type { RecipeType } from "../type/RecipeType";
 import { Status } from "../type/StatusEnum";
 import { RootState } from "./store";
-const mealType: string = "Breakfast";
 
-export const fetchSearch = createAsyncThunk("data/fetchSearch", async () => {
-    const { data } = await axios.get(
-        `https://api.edamam.com/api/recipes/v2?type=public&app_id=f5c340d8&app_key=fba58c05fd7410ca5bc1cd6cc3825eac&calories=0-150&health=alcohol-free&imageSize=LARGE&excluded=drinks&time=5%2B&mealType=Breakfast`
-    );
-    return data.hits.map(({ recipe }: any) => recipe) as RecipeType[];
-});
+export const fetchSearch = createAsyncThunk(
+    "data/fetchSearch",
+    async ({
+        mealType,
+        searchInput,
+        time
+    }: {
+        mealType: string[];
+        searchInput: string;
+        time?: string
+    }) => {
+        const { data } = await axios.get(
+            `https://api.edamam.com/api/recipes/v2?type=public${
+                "&q=" + searchInput
+            }&app_id=f5c340d8&app_key=fba58c05fd7410ca5bc1cd6cc3825eac&calories=0-150&health=alcohol-free&imageSize=LARGE&excluded=drinks${time ? '&time=' + time : '&time=5%2B'}${mealType.join(
+                ""
+            )}`
+        );
+        return data.hits.map(({ recipe }: any) => recipe) as RecipeType[];
+    }
+);
 
 interface ISearch {
     data: RecipeType[];
+    mealType: string[];
+    time: string;
+    searchInput: string;
     status: Status;
 }
 
 const initialState: ISearch = {
     data: [],
+    mealType: [],
+    time: "",
+    searchInput: "",
     status: Status.FULFILLED,
 };
 
 const searchSlice = createSlice({
     name: "search",
     initialState,
-    reducers: {},
+    reducers: {
+        setMealType: (state, action: PayloadAction<string>) => {
+            state.mealType.push("&mealType=" + action.payload);
+            console.log("add: " + state.mealType);
+        },
+        daleteItemMealType: (state, action: PayloadAction<string>) => {
+            state.mealType = state.mealType.filter(
+                (item) => item !== "&mealType=" + action.payload
+            );
+            console.log("delete: " + state.mealType);
+        },
+        setSearchInput: (state, action: PayloadAction<string>) => {
+            state.searchInput = action.payload;
+        },
+        setTime: (state, action: PayloadAction<string>) => {
+            state.time = action.payload
+        }
+    },
     extraReducers: (builder) => {
         builder.addCase(fetchSearch.pending, (state) => {
             state.status = Status.PENDING;
@@ -42,6 +79,8 @@ const searchSlice = createSlice({
         });
     },
 });
+export const { setMealType, daleteItemMealType, setSearchInput, setTime } =
+    searchSlice.actions;
 export const selectSearch = (state: RootState) => state.searchSlice;
 
 export default searchSlice.reducer;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,48 +1,99 @@
-import React, { useEffect, useState } from "react";
-import search from "../assets/search.png";
+import React, { useEffect, useState } from "react"
 import style from "../css/Search.module.css";
 import Card from "../Components/Card";
 import close from "../assets/close.png";
 import { useAppDispatch, useAppSelector } from "../Redux/hooks";
-import { fetchSearch, selectSearch } from "../Redux/searchSlice";
+import {
+    fetchSearch,
+    selectSearch,
+    setSearchInput,
+} from "../Redux/searchSlice";
+import Sort from "../Components/Sort";
+import { mealTypeArry, timeArry } from "../static/StaticData";
+import { setMealType, daleteItemMealType, setTime } from "../Redux/searchSlice";
+import { debounce } from "lodash";
 export default function Search() {
     const dispatch = useAppDispatch();
-    const [value, setValue] = useState<string>("");
-    const { data } = useAppSelector(selectSearch);
+    const { data, mealType, searchInput, time } = useAppSelector(selectSearch);
+
     useEffect(() => {
-        dispatch(fetchSearch());
-    }, [dispatch]);
+        dispatch(fetchSearch({ mealType, searchInput, time }));
+    }, [mealType, time]);
+
+    const hendleTime = debounce((value) => {
+        value && dispatch(setTime(value));
+    }, 300);
+    const clearTime = debounce((value) => {
+        value && dispatch(setTime(""));
+    }, 300);
+
+    const hendleMealType = debounce((title) => {
+        dispatch(setMealType(title));
+    }, 300);
+    const clearMealType = debounce((title) => {
+        dispatch(daleteItemMealType(title));
+    }, 300);
+
     return (
         <div className={style.wrapper}>
             <div className={style.wrapper_input}>
                 <input
-                    onChange={(e) => setValue(e.target.value)}
-                    value={value}
+                    onChange={(e) => dispatch(setSearchInput(e.target.value))}
+                    value={searchInput}
                     type="text"
                     placeholder="what a you craving today"
                 />
-                {value && (
+                {searchInput && (
                     <button
                         onClick={() => {
-                            setValue("");
+                            dispatch(setSearchInput(""));
                         }}
                         className={style.clear}
                     >
                         <img src={close} alt="close" />
                     </button>
                 )}
-                <button className={style.search_button}>Search</button>
+                <button
+                    onClick={() =>
+                        dispatch(fetchSearch({ mealType, searchInput }))
+                    }
+                    className={style.search_button}
+                >
+                    Search
+                </button>
             </div>
-            <aside>
-                <h4>Meal type</h4>
-                <select name="category" id="category-id">
-                    <option value=""></option>
-                    <option value=" ">Breakfast</option>
-                </select>
-                
-            </aside>
-            interesting recipes
-            <div >{data.map((obj) => <Card {...obj}></Card>)}</div>
+            <div className={style.wrapper_search}>
+                <aside>
+                    <h4>Time</h4>
+                    <div className={style.radio_wrapper}>
+                        {timeArry.map((item) => (
+                            <Sort
+                                addItem={() => hendleTime(item.value)}
+                                deleteItem={() => clearTime(item.value)}
+                                typeInput='radio'
+                                title={item.title}
+                                value={item.value}
+                            />
+                        ))}
+                    </div>
+                    <h4>Meal type</h4>
+                    <div className={style.radio_wrapper}>
+                        {mealTypeArry.map((item) => (
+                            <Sort
+                                addItem={() => hendleMealType(item)}
+                                deleteItem={() => clearMealType(item)}
+                                title={item}
+                            />
+                        ))}
+                    </div>
+                </aside>
+
+                <div className={style.wrapper_card}>
+                    {data.map((obj) => (
+                        <Card {...obj}></Card>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
