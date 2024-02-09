7dd56bbbb089619bf9f39d08df6f5658ec1a9e7f
Your Name <durak2754@yadnex.ru>
2024-02-09 11:43:17 +0400

    add: diet, cuisineType, skeleton, _count-pagination update: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Recipe from "./pages/Recipe";
@@ -6,16 +6,38 @@ import MainLayout from "./Layout/MainLayout";
 import NotFound from "./pages/NotFound";
 import Search from "./pages/Search";
 import style from "./css/App.module.css";
+import { setStatusHeader } from "./Redux/homeSlice";
+import { useAppDispatch } from "./Redux/hooks";
 function App() {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                dispatch(setStatusHeader("small"));
+                
+            } else {
+                dispatch(setStatusHeader("big"));
+            }
+        };
+
+        document.addEventListener("scroll", handleScroll);
+
+        return () => {
+            document.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <Routes>
-            <Route path="/" element={<MainLayout></MainLayout>}>
-                <Route path="" element={<Home></Home>}></Route>
-                <Route path="Recipe" element={<Recipe></Recipe>}></Route>
-                <Route path="Search" element={<Search></Search>}></Route>
-            </Route>
-            <Route path="*" element={<NotFound></NotFound>}></Route>
-        </Routes>
+        <div>
+            <Routes>
+                <Route path="/" element={<MainLayout></MainLayout>}>
+                    <Route path="" element={<Home></Home>}></Route>
+                    <Route path="Recipe" element={<Recipe></Recipe>}></Route>
+                    <Route path="Search" element={<Search></Search>}></Route>
+                </Route>
+                <Route path="*" element={<NotFound></NotFound>}></Route>
+            </Routes>
+        </div>
     );
 }
 
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,19 +7,22 @@ import { useAppDispatch, useAppSelector } from "../Redux/hooks";
 import { selectSearch } from "../Redux/searchSlice";
 import { setSearchInput } from "../Redux/searchSlice";
 import { fetchSearch } from "../Redux/searchSlice";
+import { selectHome } from "../Redux/homeSlice";
 
 const Header = () => {
     const dispatch = useAppDispatch();
-    const { searchInput, mealType, dishType } = useAppSelector(selectSearch);
+    const { searchInput, mealType, dishType, diet, cuisineType } =
+        useAppSelector(selectSearch);
+    const { statusHeader } = useAppSelector(selectHome);
     const { pathname } = useLocation();
     return (
-        <header className={style.header_wrapper}>
+        <header className={statusHeader === "big" ? style.header_wrapper : style.header_wrapper_small}>
             <div className={style.container}>
                 <Link to="/">
-                    <img className={style.logo} src={logo} alt="" />
+                    <img className={statusHeader === "small" && style.logo} src={logo} alt="" />
                 </Link>
                 <div className={style.search}>
-                    {pathname !== "/Search" && (
+                    {statusHeader === "small" || pathname !== "/Search" && (
                         <input
                             onChange={(e) =>
                                 dispatch(setSearchInput(e.target.value))
@@ -32,10 +35,18 @@ const Header = () => {
                     <Link to="Search">
                         <button
                             onClick={() =>
-                                dispatch(fetchSearch({ mealType, dishType, searchInput }))
+                                dispatch(
+                                    fetchSearch({
+                                        mealType,
+                                        dishType,
+                                        searchInput,
+                                        diet,
+                                        cuisineType,
+                                    })
+                                )
                             }
                         >
-                            <img src={search} alt="" />
+                            <img className={statusHeader === "small" && style.search_button} src={search} alt="" />
                         </button>
                     </Link>
                 </div>
