e3854b9ca6e22f5431704633f23fffeb68866d5f
Your Name <durak2754@yadnex.ru>
2024-02-12 22:41:48 +0400

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Recipe from "./pages/Recipe";
 import MainLayout from "./Layout/MainLayout";
-import NotFound from "./pages/NotFound";
-import Search from "./pages/Search";
 import style from "./css/App.module.css";
 import { setStatusHeader } from "./Redux/homeSlice";
 import { useAppDispatch } from "./Redux/hooks";
-import RecipesByCategory from "./pages/RecipesByCategory";
+
+
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const Recipe = React.lazy(() => import("./pages/Recipe"));
+const Search = React.lazy(() => import("./pages/Search"));
+const RecipesByCategory = React.lazy(() => import("./pages/RecipesByCategory"));
+
 function App() {
     const dispatch = useAppDispatch();
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 100) {
-                dispatch(setStatusHeader("small"));
-                
+                dispatch(setStatusHeader("small")); 
             } else {
                 dispatch(setStatusHeader("big"));
             }
@@ -33,7 +35,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainLayout></MainLayout>}>
                     <Route path="" element={<Home></Home>}></Route>
-                    <Route path="RecipesByCategory" element={<RecipesByCategory></RecipesByCategory>}></Route>
+                    <Route
+                        path="RecipesByCategory"
+                        element={<RecipesByCategory></RecipesByCategory>}
+                    ></Route>
                     <Route path="Recipe" element={<Recipe></Recipe>}></Route>
                     <Route path="Search" element={<Search></Search>}></Route>
                 </Route>
